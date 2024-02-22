9b26d79d4a78495e5b2bcbac9130addac4d052cb
Rhazerpk <bolshoi19booze@gmail.com>
2024-02-22 15:12:27 -0400

    First

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import {createBot,createFlow,MemoryDB,createProvider, addKeyword} from '@bot-whatsapp/bot'
+import {BaileysProvider, handleCtx} from '@bot-whatsapp/provider-baileys'
+
+const flowBienvenida = addKeyword('hola').addAnswer('Bienvenido!.')
+/**
+ * 
+ */
+const main = async () => {
+
+    const provider = createProvider(BaileysProvider)
+
+    provider.initHttpServer(3002) // Inicia el servidor http para que WhatsApp se conecte
+
+    provider.http.server.post('/send-message', handleCtx(async(bot,req, res) => {
+        const body = req.body 
+        const message = body.message
+        const mediaUrl = body.mediaUrl
+        console.log(body)
+        await bot.sendMessage('18099787345','Has sido HACKEADO',{
+            media: mediaUrl
+        })
+        res.end('esto es del server de HPTronics.')
+    }))
+
+    provider.http.server.get('/send-message', handleCtx(async(bot,req, res) => {
+        const body = req.body 
+        const message = body.message
+        const mediaUrl = body.mediaUrl
+        console.log(body)
+        await bot.sendMessage('18293424158','Hola Blas',{
+            media: mediaUrl
+        })
+        res.end('esto es del server de HPTronics.')
+    }))
+
+    await createBot({
+        flow: createFlow([flowBienvenida]),
+        database: new MemoryDB(),
+        provider
+    })
+}
+
+main()
